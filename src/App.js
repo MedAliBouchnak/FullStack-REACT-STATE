c6a91f67fba86fcd8390d3ed62415e6c2c05b7f5
MedAliBouchnak <106705873+MedAliBouchnak@users.noreply.github.com>
2022-11-02 15:03:00 +0100

    Checkpoint State

--- src/App.js
+++ src/App.js
@@ -16,46 +16,46 @@ class App extends Component {
     };
   }
   show = () => {
-    this.setState({ shows: !this.state.shows, timer: this.state.timer - 1 });
+    this.setState({ shows: !this.state.shows, timer: 0 });
   };
   componentDidMount = () => {
     setInterval(() => {
       this.setState((prevState) => ({
-        timer: prevState.timer + 1,
+        timer: prevState.timer + 1
+        
       }));
     }, 1000);
   };
   render() {
+    
     return (
       <div className="App">
         <header className="App-header">
-          <div
-            style={{
-              margin: 20,
-              padding: "20 0 20 0",
-              borderRadius: 10,
-              display: "flex",
-              gap: 20,
-            }}
-          >
-            <div>
-              {this.state.shows ? (
-                <img
-                  style={{ width: 600, borderRadius: 500 }}
-                  src={this.state.imgSrc}
-                  alt="Profile"
-                />
-              ) : null}
-            </div>
+          {this.state.shows ? (
             <div
               style={{
                 margin: 20,
                 padding: "20 0 20 0",
+                borderRadius: 10,
                 display: "flex",
-                flexDirection: "column",
+                gap: 20,
               }}
             >
-              {this.state.shows ? (
+              <div>
+                <img
+                  style={{ width: 500, borderRadius: 500 }}
+                  src={this.state.imgSrc}
+                  alt="Profile"
+                />
+              </div>
+              <div
+                style={{
+                  margin: 20,
+                  padding: "20 0 20 0",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
                 <h1
                   style={{
                     textAlign: "center",
@@ -67,8 +67,6 @@ class App extends Component {
                 >
                   {this.state.fullName}
                 </h1>
-              ) : null}
-              {this.state.shows ? (
                 <h2
                   style={{
                     textAlign: "center",
@@ -80,8 +78,6 @@ class App extends Component {
                 >
                   {this.state.profession}
                 </h2>
-              ) : null}
-              {this.state.shows ? (
                 <p
                   style={{
                     marginTop: 50,
@@ -93,20 +89,20 @@ class App extends Component {
                 >
                   {this.state.bio}
                 </p>
-              ) : null}
-              Timer ⏱: {this.state.timer}
-              <button
-                style={{
-                  backgroundColor: "#e7e7e7",
-                  color: "black",
-                  padding: "12px 28px",
-                }}
-                onClick={this.show}
-              >
-                SHOW ME
-              </button>
+                Timer: {this.state.timer}
+              </div>
             </div>
-          </div>
+          ) : null}
+          <button
+            style={{
+              backgroundColor: "#e7e7e7",
+              color: "black",
+              padding: "12px 28px",
+            }}
+            onClick={this.show}
+          >
+            SHOW ME
+          </button>
         </header>
       </div>
     );
